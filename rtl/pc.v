9001ae70a1e20734f8bf9c4292335db0f7484add
Venkata sukumar reddy Ch <136838784+sukumarreddy1526@users.noreply.github.com>
2026-03-16 23:05:24 +0530

    Create pc.v module for program counter

--- rtl/pc.v
+++ rtl/pc.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02.03.2026 15:12:53
-// Design Name: 
-// Module Name: pc
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 
 module pc(
     input wire clk,
