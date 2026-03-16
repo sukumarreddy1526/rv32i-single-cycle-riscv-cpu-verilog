f9031313abb230644588fb10f0682d10bb1aeebd
Venkata sukumar reddy Ch <136838784+sukumarreddy1526@users.noreply.github.com>
2026-03-16 23:04:36 +0530

    Add cpu_top module with basic CPU architecture
    Removed unnecessary header comments in cpu_top.v

--- rtl/cpu_top.v
+++ rtl/cpu_top.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.03.2026 14:36:30
-// Design Name: 
-// Module Name: cpu_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module cpu_top(
     input wire clk,
